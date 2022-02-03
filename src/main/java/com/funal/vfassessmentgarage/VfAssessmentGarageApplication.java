74b625f0574eb33200ac0a918f79f1682f8387c1
Furkan Unal <furkan.unal@mera.com>
2022-02-03 21:37:01 +0300

    init commit

---
+++ src/main/java/com/funal/vfassessmentgarage/VfAssessmentGarageApplication.java
@@ -0,0 +1,13 @@
+package com.funal.vfassessmentgarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfAssessmentGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VfAssessmentGarageApplication.class, args);
+    }
+
+}
